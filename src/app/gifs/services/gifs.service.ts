f43c99fa3b62e13070ddebc5285ffa34f71db5d1
Christian Flores <christ.flores.22@gmail.com>
2021-05-21 13:12:05 -0500

    Capitulo 90

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,34 +1,56 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private _apiKey: string = 'wYlAiVNGQGVe0BeRHhjBAzVjNVpNEzEy';
+  private _servicioUrl:string ='https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
-
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(){
     
     return [...this._historial];
   }
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    //1ra forma
+    // if (localStorage.getItem('historial')){
+    //   this._historial = JSON.parse((localStorage.getItem('historial')!));
+    // }
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
+
   buscarGifs( query:string){
     query = query.trim().toLowerCase();
     if(!this._historial.includes( query )){
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=wYlAiVNGQGVe0BeRHhjBAzVjNVpNEzEy&q=${ query }&limit=10`)
-        .subscribe( ( resp:any ) => {
+    const params = new HttpParams().
+      set('api_key',this._apiKey).
+      set('limit','10').
+      set('q', query);
+      
+
+    this.http.get<SearchGifsResponse>(`${ this._servicioUrl }/search`, { params })
+        .subscribe( ( resp : SearchGifsResponse ) => {
             console.log( resp.data );
             this.resultados = resp.data;
+            localStorage.setItem('resultados',JSON.stringify(this.resultados));
           }
         )
   }
+
+
+
+
+
 
   // async buscarGifs( query:string){
   //   query = query.trim().toLowerCase();
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,5 +13,8 @@ export class SidebarComponent{
 
 
   constructor( private gifsService:GifsService) { }
+  buscar(termino:string){
+      this.gifsService.buscarGifs(termino);
+  }
 
 }
